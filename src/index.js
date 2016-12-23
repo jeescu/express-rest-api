c1f9d1841dfcacc828670119c8bacd5e5fec823c
John Edward <john.escuyos@98labs.com>
2016-12-23 17:39:03 +0800

    initial code from express-rest-api by james

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import initializeDb from './db';
+import middleware from './middleware';
+import api from './api';
+import config from './config.json';
+
+let app = express();
+app.server = http.createServer(app);
+
+// 3rd party middleware
+app.use(cors({
+	exposedHeaders: config.corsHeaders
+}));
+
+app.use(bodyParser.json({
+	limit : config.bodyLimit
+}));
+
+// connect to db
+initializeDb( db => {
+
+	// internal middleware
+	app.use(middleware({ config, db }));
+
+	// api router
+	app.use('/api', api({ config, db }));
+
+	app.server.listen(process.env.PORT || config.port);
+
+	console.log(`Started on port ${app.server.address().port}`);
+});
+
+export default app;
