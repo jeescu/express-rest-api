6d14c849d8acc0a522193666085afd68a7bf57be
John Edward <john.escuyos@98labs.com>
2017-05-02 23:34:52 +0800

    added sample e2e test

---
+++ src/lib/utils/userToken.spec.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { getTokenForUser } from './userToken';
+
+describe('User Token util', () => {
+    it('should get token for user', () => {
+        expect(getTokenForUser({})).to.not.equal('');
+    });
+});
---
+++ test/e2e/auth.spec.js
@@ -0,0 +1,16 @@
+import request from 'supertest';
+import app from '../../src';
+
+describe('Authentication', () => {
+    describe('Sign In', () => {
+        it('should return unauthorized for invalid user', () => {
+            request(app)
+                .post('/auth/signin')
+                .send({ email: 'anonymous@mail.co', password: '123' })
+                .expect(401)
+                .end((err, res) => {
+                    if (err) throw err;
+                });
+        });
+    });
+});
